66872cadd42137ff51b0dffdaa704f36ac11b3ed
reinx058 <reinx058@iastate.edu>
2022-05-23 20:17:56 -0500

    chore: setup github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/personal-website',
+  assetPrefix: '/personal-website',
 }
 
 module.exports = nextConfig
