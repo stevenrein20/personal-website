4cc8914234fb125ec9ebb124733d2a061bcf848d
Steven Rein <39576275+stevenrein20@users.noreply.github.com>
2022-05-24 21:12:44 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/personal-website',
-  assetPrefix: '/personal-website',
 }
 
 module.exports = nextConfig
